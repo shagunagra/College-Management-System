9f37d39fd0c86af51fde731f6d81cda1c1e83325
shagunagra <shagunagrawal1998@gmail.com>
2020-07-06 23:44:06 -0600

    Updates

---
+++ precache-manifest.a7c6c6b2ec38ba5734bdab793b874987.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05941bdd22261735aa7fa00331d588b",
+    "url": "/College-Management-System/index.html"
+  },
+  {
+    "revision": "106c4e2464180a17e8c2",
+    "url": "/College-Management-System/static/css/main.59b0c29b.chunk.css"
+  },
+  {
+    "revision": "fcb388aff4d80746ebdb",
+    "url": "/College-Management-System/static/js/2.c1ef9141.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/College-Management-System/static/js/2.c1ef9141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "106c4e2464180a17e8c2",
+    "url": "/College-Management-System/static/js/main.35aa3a3a.chunk.js"
+  },
+  {
+    "revision": "aa754cd529e024564c9f",
+    "url": "/College-Management-System/static/js/runtime-main.543bb321.js"
+  },
+  {
+    "revision": "3fe77d823ec3cf667ab535446bace674",
+    "url": "/College-Management-System/static/media/50-Beautiful-and-Minimalist-Presentation-Backgrounds-042.3fe77d82.jpg"
+  },
+  {
+    "revision": "ae5f1961714e6879c12a3b4bb0a5f39f",
+    "url": "/College-Management-System/static/media/project-proposal-76f0a3e70715ab89442aa753fee01c842643fd3567e49c6083e0664f5b74bbf3.ae5f1961.png"
+  }
+]);
